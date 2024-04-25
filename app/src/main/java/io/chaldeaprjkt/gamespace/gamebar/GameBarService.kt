954fd70f1cbeedbef88bfe81cb65f4515b4daa3d
Pranav Vashi <neobuddy89@gmail.com>
2024-04-26 00:36:37 +0530

    GameSpace: Apply opacity settings to menu switcher and game bar
    Fixes: https://github.com/crdroidandroid/issue_tracker/issues/377
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- app/src/main/java/io/chaldeaprjkt/gamespace/gamebar/GameBarService.kt
+++ app/src/main/java/io/chaldeaprjkt/gamespace/gamebar/GameBarService.kt
@@ -141,7 +141,9 @@ class GameBarService : Hilt_GameBarService() {
         rootBarView = LayoutInflater.from(this)
             .inflate(R.layout.window_util, frame, false)!!
         barView = rootBarView.findViewById(R.id.container_bar)!!
+        barView.alpha = appSettings.menuOpacity / 100f
         menuSwitcher = rootBarView.findViewById(R.id.action_menu_switcher)!!
+        menuSwitcher.alpha = appSettings.menuOpacity / 100f
         danmakuService.init()
     }
 
@@ -342,6 +344,7 @@ class GameBarService : Hilt_GameBarService() {
 
     private fun panelButton() {
         val actionPanel = rootBarView.findViewById<ImageButton>(R.id.action_panel)!!
+        actionPanel.alpha = appSettings.menuOpacity / 100f
         actionPanel.setOnClickListener {
             showPanel = !showPanel
         }
@@ -349,6 +352,7 @@ class GameBarService : Hilt_GameBarService() {
 
     private fun screenshotButton() {
         val actionScreenshot = rootBarView.findViewById<ImageButton>(R.id.action_screenshot)!!
+        actionScreenshot.alpha = appSettings.menuOpacity / 100f
         actionScreenshot.setOnClickListener {
             takeShot()
         }
@@ -356,6 +360,7 @@ class GameBarService : Hilt_GameBarService() {
 
     private fun recorderButton() {
         val actionRecorder = rootBarView.findViewById<ImageButton>(R.id.action_record)!!
+        actionRecorder.alpha = appSettings.menuOpacity / 100f
         val recorder = screenUtils.recorder ?: let { actionRecorder?.isVisible = false; return }
         recorder.addRecordingCallback(object : IRecordingCallback.Stub() {
             override fun onRecordingStart() {
